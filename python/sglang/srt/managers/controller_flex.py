c22d133161850accd530e8544226a384b0c52fba
YouYangxiu <yyx1824665362@gmail.com>
2024-08-16 21:23:30 +0800

    1

--- python/sglang/srt/managers/controller_flex.py
+++ python/sglang/srt/managers/controller_flex.py
@@ -151,35 +151,37 @@ class ControllerMultiFlex:
             return
         remained_token = [k.value for k in self.controller_info.current_bs]
         available_mem = [k.value for k in self.controller_info.available_kv_cache]
+        dealt = []
+        for i in range (len(available_mem)):
+            dealt.append(available_mem[i] - remained_token[i])
+        print(dealt)
         for r in input_requests:
             input_len = len(r.input_ids)
-            print(input_len)
-            if input_len <= max(available_mem):
+        #     print(input_len)
+        #     if input_len <= max(available_mem):
                 
-                # dealt = available_mem - remained_token
+        #         # 说明有mem可以放，放在满足mem且未处理最少的那个机器上
                 
-                # 说明有mem可以放，放在满足mem且未处理最少的那个机器上
+        #         target_gpu = -1
+        #         min_remain_token_tmp = 1e20
+        #         for i, mem in enumerate(available_mem):
+        #             if mem >= input_len:
+        #                 # 更新最小        
+        #                 if min_remain_token_tmp > remained_token[i]:
+        #                     min_remain_token_tmp =  remained_token[i]
+        #                     target_gpu = i  
                 
-                target_gpu = -1
-                min_remain_token_tmp = 1e20
-                for i, mem in enumerate(available_mem):
-                    if mem >= input_len:
-                        # 更新最小        
-                        if min_remain_token_tmp > remained_token[i]:
-                            min_remain_token_tmp =  remained_token[i]
-                            target_gpu = i  
-                
-                assert target_gpu != -1, "Can not find target gpu, please check che code"
-                self.workers[target_gpu].queue.put(r)
-                remained_token[target_gpu] += input_len
-                available_mem[target_gpu] -= input_len        
+        #         assert target_gpu != -1, "Can not find target gpu, please check che code"
+        #         self.workers[target_gpu].queue.put(r)
+        #         remained_token[target_gpu] += input_len
+        #         available_mem[target_gpu] -= input_len        
                     
-            else:
-                #这就说明没有合适的调度
-                index = remained_token.index(min(remained_token))
-                self.workers[index].queue.put(r)
-                remained_token[index] += input_len
-                available_mem[index] -= input_len        
+        #     else:
+        #         #这就说明没有合适的调度
+            index = remained_token.index(min(remained_token))
+            self.workers[index].queue.put(r)
+            remained_token[index] += input_len
+            available_mem[index] -= input_len        
         with self.controller_info.lock:
             for i, v in enumerate(remained_token):
                 self.controller_info.current_bs[i].value = v
