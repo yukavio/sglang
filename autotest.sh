30937eb9e1166adab60ca02443089a29b9d7df33
YouYangxiu <yyx1824665362@gmail.com>
2024-08-21 18:53:34 +0800

    fix bash

--- autotest.sh
+++ autotest.sh
@@ -9,7 +9,7 @@ echo "====================== $(date) ======================" >> "$LOG_FILE"
 # 启动服务并将其放到后台，重定向输出到日志文件
 /data/home/josephyou/WXG_WORK/bin/micromamba run -n sglang python3 -m sglang.launch_server --model-path Qwen/Qwen2-7B \
     --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
-    --dp-size 8 --load-balance-method resources_aware \
+    --dp-size 1 --load-balance-method resources_aware \
     --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
 
 # 获取服务的进程ID
@@ -28,6 +28,6 @@ for i in {1..10}; do
         --random-output-len 1024 --random-input-len 4096 \
         --random-range-ratio 0.5 --seed 1234 \
         --num-prompts 2 --request-rate 15.7 >> "$LOG_FILE" 2>&1
-    time.sleep(30)
+    sleep 30
 
 
