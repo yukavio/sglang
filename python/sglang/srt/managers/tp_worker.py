42f667030d9e0ea7c50c6e0211cb0f5d4de25eff
YouYangxiu <yyx1824665362@gmail.com>
2024-08-26 16:37:38 +0800

    11

--- python/sglang/srt/managers/tp_worker.py
+++ python/sglang/srt/managers/tp_worker.py
@@ -277,8 +277,8 @@ class ModelTpServer:
 
                     # Print stats
                     if self.tp_rank == 0 and self.decode_forward_ct % 1 == 0:
-                        self.print_decode_stats()
-                        # pass
+                        # self.print_decode_stats()
+                        pass
 
                     if self.running_batch.is_empty():
                         self.running_batch = None
