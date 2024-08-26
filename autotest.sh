a150fae1014dc504dd36555a4753f492d2b8e66c
YouYangxiu <yyx1824665362@gmail.com>
2024-08-26 10:37:01 +0800

    1

--- autotest.sh
+++ autotest.sh
@@ -13,10 +13,6 @@ echo "====================== $(date) ======================" >> "$LOG_FILE"
     --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
 
 
-    
-# 获取服务的进程ID
-SERVICE_PID=$!
-
 # 等待一段时间以确保服务启动
 sleep 500
 
@@ -27,11 +23,12 @@ sleep 500
         --tokenizer Qwen/Qwen1.5-32B --model Qwen/Qwen1.5-32B \
         --random-output-len 1024 --random-input-len 4096 \
         --random-range-ratio 0.5 --seed 1234 \
-        --num-prompts 20000 --request-rate 1.4 >> "$LOG_FILE" 2>&1
+        --num-prompts 100 --request-rate 1.4 >> "$LOG_FILE" 2>&1
 
-
-kill -9 $SERVICE_PID
-sleep 300
+# 获取服务的进程ID
+sleep 100
+ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
+sleep 100
 
 
 # 启动服务并将其放到后台，重定向输出到日志文件
@@ -39,8 +36,7 @@ sleep 300
     --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
     --dp-size 4 --tp-size 2 --load-balance-method resources_aware \
     --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
-# 获取服务的进程ID
-SERVICE_PID=$!
+
 sleep 500
 
 /workspace/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
@@ -48,26 +44,31 @@ sleep 500
         --tokenizer Qwen/Qwen1.5-32B --model Qwen/Qwen1.5-32B \
         --random-output-len 1024 --random-input-len 4096 \
         --random-range-ratio 0.5 --seed 1234 \
-        --num-prompts 20000 --request-rate 1.4 >> "$LOG_FILE" 2>&1
-kill -9 $SERVICE_PID
-sleep 300
-
-
+        --num-prompts 100 --request-rate 1.4 >> "$LOG_FILE" 2>&1
+# 获取服务的进程ID
+sleep 100
+ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
+sleep 100
 
 # 启动服务并将其放到后台，重定向输出到日志文件
 /workspace/bin/micromamba run -n sglang python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-32B \
     --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
     --dp-size 2 --tp-size 4 --load-balance-method resources_aware \
-    --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
-# 获取服务的进程ID
-SERVICE_PID=$!
-sleep 500
-
-/workspace/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
         --host 127.0.0.1 --port 8080 --dataset-name random \
         --tokenizer Qwen/Qwen1.5-32B --model Qwen/Qwen1.5-32B \
         --random-output-len 1024 --random-input-len 4096 \
         --random-range-ratio 0.5 --seed 1234 \
-        --num-prompts 20000 --request-rate 1.4 >> "$LOG_FILE" 2>&1
-kill -9 $SERVICE_PID
-sleep 300
+        --num-prompts 100 --request-rate 1.4 >> "$LOG_FILE" 2>&1
+
+sleep 500
+
+/workspace/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
+                --host 127.0.0.1 --port 8080 --dataset-name random \
+                --tokenizer Qwen/Qwen1.5-32B --model Qwen/Qwen1.5-32B \
+                --random-output-len 1024 --random-input-len 4096 \
+                --random-range-ratio 0.5 --seed 1234 \
+                --num-prompts 100 --request-rate 1.4 >> "$LOG_FILE" 2>&1
+# 获取服务的进程ID
+sleep 100
+ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
+sleep 100
