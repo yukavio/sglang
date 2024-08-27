4b7ecf4be44aa15a6e4a183f3d110c2235130e9b
YouYangxiu <yyx1824665362@gmail.com>
2024-08-27 14:58:56 +0800

    1

--- autotest.sh
+++ autotest.sh
@@ -19,16 +19,16 @@ sleep 300
 
 # 启动 benchmark 脚本，重定向输出到日志文件
 # 循环从 7 到 12，步进为 0.5
-for rate in $(seq 3.5 0.25 6); do
-    echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
-    /home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
-            --host 127.0.0.1 --port 8080 --dataset-name random \
-            --tokenizer Qwen/Qwen2-7B --model Qwen/Qwen2-7B \
-            --random-output-len 1024 --random-input-len 4096 \
-            --random-range-ratio 0.5 --seed 1234 \
-            --num-prompts 5000 --request-rate $rate >> "$LOG_FILE" 2>&1
-    sleep 100
-done
+# for rate in $(seq 3.5 0.25 6); do
+# echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
+/home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
+        --host 127.0.0.1 --port 8080 --dataset-name random \
+        --tokenizer Qwen/Qwen2-7B --model Qwen/Qwen2-7B \
+        --random-output-len 1024 --random-input-len 4096 \
+        --random-range-ratio 0.5 --seed 1234 \
+        --num-prompts 5000 --request-rate 4.5 >> "$LOG_FILE" 2>&1
+sleep 100
+# done
 ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
 sleep 100
 
@@ -43,16 +43,16 @@ echo "Running with setting: dp=2 tp=2 ==========================================
     --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
 
 sleep 300
-for rate in $(seq 3.5 0.25 6); do
-    echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
-    /home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
-            --host 127.0.0.1 --port 8080 --dataset-name random \
-            --tokenizer Qwen/Qwen2-7B --model Qwen/Qwen2-7B \
-            --random-output-len 1024 --random-input-len 4096 \
-            --random-range-ratio 0.5 --seed 1234 \
-            --num-prompts 5000 --request-rate $rate >> "$LOG_FILE" 2>&1
-    sleep 100
-done
+# for rate in $(seq 3.5 0.25 6); do
+    # echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
+/home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
+        --host 127.0.0.1 --port 8080 --dataset-name random \
+        --tokenizer Qwen/Qwen2-7B --model Qwen/Qwen2-7B \
+        --random-output-len 1024 --random-input-len 4096 \
+        --random-range-ratio 0.5 --seed 1234 \
+        --num-prompts 5000 --request-rate 4.5 >> "$LOG_FILE" 2>&1
+sleep 100
+# done
 ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
 sleep 100
 
@@ -66,16 +66,16 @@ echo "Running with setting: dp=1 tp=4 ==========================================
     --dp-size 1 --tp-size 4 --load-balance-method resources_aware \
     --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
 sleep 300
-for rate in $(seq 3.5 0.25 6); do
-    echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
-    /home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
-                    --host 127.0.0.1 --port 8080 --dataset-name random \
-                    --tokenizer Qwen/Qwen2-7B --model Qwen/Qwen2-7B \
-                    --random-output-len 1024 --random-input-len 4096 \
-                    --random-range-ratio 0.5 --seed 1234 \
-                    --num-prompts 5000 --request-rate $rate >> "$LOG_FILE" 2>&1
+# for rate in $(seq 3.5 0.25 6); do
+    # echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
+/home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
+                --host 127.0.0.1 --port 8080 --dataset-name random \
+                --tokenizer Qwen/Qwen2-7B --model Qwen/Qwen2-7B \
+                --random-output-len 1024 --random-input-len 4096 \
+                --random-range-ratio 0.5 --seed 1234 \
+                --num-prompts 5000 --request-rate 4.5 >> "$LOG_FILE" 2>&1
     sleep 100
-done
+# done
 ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
 sleep 100
 
