d506c905b12111d81872a6d1523bcc3a4ebccc3b
YouYangxiu <yyx1824665362@gmail.com>
2024-08-28 18:30:57 +0800

    1

--- autotest.sh
+++ autotest.sh
@@ -6,31 +6,31 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 # 在日志文件中写入分隔符和当前时间
 echo "====================== $(date) ======================" >> "$LOG_FILE"
 
-# 启动服务并将其放到后台，重定向输出到日志文件
-echo "Running with setting: dp=4 tp=1 ======================================================" >> "$LOG_FILE"
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
-    --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
-    --dp-size 4 --load-balance-method resources_aware \
-    --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
+# # 启动服务并将其放到后台，重定向输出到日志文件
+# echo "Running with setting: dp=4 tp=1 ======================================================" >> "$LOG_FILE"
+# /home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
+#     --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
+#     --dp-size 4 --load-balance-method resources_aware \
+#     --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
 
 
-# 等待一段时间以确保服务启动
-sleep 300
+# # 等待一段时间以确保服务启动
+# sleep 300
 
-# 启动 benchmark 脚本，重定向输出到日志文件
-# 循环从 7 到 12，步进为 0.5
-# for rate in $(seq 3.5 0.25 6); do
-# echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
-        --host 127.0.0.1 --port 8080 --dataset-name random \
-        --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
-        --random-output-len 1024 --random-input-len 4096 \
-        --random-range-ratio 0.5 --seed 1234 \
-        --num-prompts 5000 --request-rate 0.25 >> "$LOG_FILE" 2>&1
-sleep 100
-# done
-ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
-sleep 100
+# # 启动 benchmark 脚本，重定向输出到日志文件
+# # 循环从 7 到 12，步进为 0.5
+# # for rate in $(seq 3.5 0.25 6); do
+# # echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
+# /home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
+#         --host 127.0.0.1 --port 8080 --dataset-name random \
+#         --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
+#         --random-output-len 1024 --random-input-len 4096 \
+#         --random-range-ratio 0.5 --seed 1234 \
+#         --num-prompts 5000 --request-rate 0.25 >> "$LOG_FILE" 2>&1
+# sleep 100
+# # done
+# ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
+# sleep 100
 
 #=============================================================================================================================================================
 
@@ -45,12 +45,12 @@ echo "Running with setting: dp=2 tp=2 ==========================================
 sleep 300
 # for rate in $(seq 3.5 0.25 6); do
     # echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
+/home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.iter_search_best_qps --backend sglang \
         --host 127.0.0.1 --port 8080 --dataset-name random \
         --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
         --random-output-len 1024 --random-input-len 4096 \
         --random-range-ratio 0.5 --seed 1234 \
-        --num-prompts 5000 --request-rate 0.25 >> "$LOG_FILE" 2>&1
+        --num-prompts 1000 --request-rate-list "[0.3, 0.35, 0.36, 0.37, 0.4, 0.42, 0.5]" >> "$LOG_FILE" 2>&1
 sleep 100
 # done
 ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
@@ -68,12 +68,12 @@ echo "Running with setting: dp=1 tp=4 ==========================================
 sleep 300
 # for rate in $(seq 3.5 0.25 6); do
     # echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
-                --host 127.0.0.1 --port 8080 --dataset-name random \
-                --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
-                --random-output-len 1024 --random-input-len 4096 \
-                --random-range-ratio 0.5 --seed 1234 \
-                --num-prompts 5000 --request-rate 0.25 >> "$LOG_FILE" 2>&1
+/home/qspace/workspace/josephyou/bin/micromamba run -n sglang python3 -m sglang.iter_search_best_qps --backend sglang \
+        --host 127.0.0.1 --port 8080 --dataset-name random \
+        --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
+        --random-output-len 1024 --random-input-len 4096 \
+        --random-range-ratio 0.5 --seed 1234 \
+        --num-prompts 1000 --request-rate-list "[0.3, 0.35, 0.36, 0.37, 0.4, 0.42, 0.5]" >> "$LOG_FILE" 2>&1
     sleep 100
 # done
 ps -elf | grep python  | awk '{print $4}' | xargs  kill -s 9
