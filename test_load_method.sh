d24143da903d62aaed7186a99efabd385cfa4e0d
YouYangxiu <yyx1824665362@gmail.com>
2024-09-02 14:44:43 +0800

    1

--- test_load_method.sh
+++ test_load_method.sh
@@ -135,24 +135,26 @@ settings["power_of_2_choice"]="dp8 power_of_2_choice"
 settings["round_robin"]="dp8 round_robin"
 settings["resources_aware"]="dp8 resources_aware"
 
-# 循环处理每个设置
-for method in "${!settings[@]}"; do
-    setting=${settings[$method]}
-    echo "Running with setting: $setting ======================================================" >> "$LOG_FILE"
-    
-    # 启动服务并将其放到后台，重定向输出到日志文件
-    /workspace/bin/micromamba run -n sglang python3 -m sglang.launch_server --model-path Qwen/Qwen2-7B \
-        --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
-        --dp-size 8 --load-balance-method $method \
-        --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
-    export LOAD_BALANCE_METHOD=$method
+# for rate in $(seq 16 0.1 16.2); do
+for rate in 16.0, 16.0, 16.0; do
+    # 循环处理每个设置
+    for method in "${!settings[@]}"; do
+        setting=${settings[$method]}
+        echo "Running with setting: $setting ======================================================" >> "$LOG_FILE"
+        
+        # 启动服务并将其放到后台，重定向输出到日志文件
+        /workspace/bin/micromamba run -n sglang python3 -m sglang.launch_server --model-path Qwen/Qwen2-7B \
+            --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
+            --dp-size 8 --load-balance-method $method \
+            --chunked-prefill-size 2048 --disable-radix-cache --random-seed 418729308 >> "$LOG_FILE" 2>&1 &
+        export LOAD_BALANCE_METHOD=$method
 
-    # 等待一段时间以确保服务启动
-    sleep 300
+        # 等待一段时间以确保服务启动
+        sleep 300
 
-    # 启动 benchmark 脚本，重定向输出到日志文件
-    # 循环从 16 到 20，步进为 0.1
-    for rate in $(seq 16 0.1 16.2); do
+        # for rate in $(seq 16 0.1 16.2); do
+        # 启动 benchmark 脚本，重定向输出到日志文件
+        # 循环从 16 到 20，步进为 0.1
         echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
         /workspace/bin/micromamba run -n sglang python3 -m sglang.bench_serving --backend sglang \
                 --host 127.0.0.1 --port 8080 --dataset-name random \
@@ -161,11 +163,12 @@ for method in "${!settings[@]}"; do
                 --random-range-ratio 0.5 --seed 1234 \
                 --num-prompts 20000 --request-rate $rate >> "$LOG_FILE" 2>&1
         sleep 100
+        # done
+
+        # 杀死特定的 Python 进程
+        ps -elf | grep '[p]ython' | awk '{print $4}' | xargs kill -s 9
+
+        # 等待一段时间以确保进程被杀死
+        sleep 300
     done
-
-    # 杀死特定的 Python 进程
-    ps -elf | grep '[p]ython' | awk '{print $4}' | xargs kill -s 9
-
-    # 等待一段时间以确保进程被杀死
-    sleep 300
 done
