64ea1d12b4cb055566eafaf01d61dc83c0d15fe8
josephyou <josephyou@tencent.com>
2025-12-01 19:59:42 +0800

    update tp main2

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -507,7 +507,12 @@ class ModelRunner:
         if self.device == "cuda":
             self.init_cublas()
             self.init_attention_backend()
-            self.init_device_graphs()
+            if self.spec_algorithm.is_simple_eagle():
+                self.graph_runner = None
+                self.graph_mem_usage = 0
+                logger.info("We init target model cuda graphs in simple eagle....")
+            else:
+                self.init_device_graphs()
         elif self.device in ["npu", "cpu"]:
             self.init_attention_backend()
             self.init_device_graphs()
@@ -2240,6 +2245,7 @@ class ModelRunner:
             mode_check()
             and self.graph_runner
             and self.graph_runner.can_run(forward_batch)
+            and not self.spec_algorithm.is_simple_eagle() # Simple eagle use own cuda graph in simple_eagle_cuda_graph_runner
         )
 
         if can_run_graph:
@@ -2256,7 +2262,10 @@ class ModelRunner:
         else:
             forward_batch.prepare_attn_tp_scatter_input(self)
 
-        if forward_batch.forward_mode.is_decode():
+        if (
+            forward_batch.forward_mode.is_decode()
+            or forward_batch.forward_mode.is_simple_verify()
+        ):
             ret = self.forward_decode(
                 forward_batch,
                 skip_attn_backend_init=skip_attn_backend_init,
