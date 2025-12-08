d35138a1b995145601d23406227173c2de06a32c
KuangjuX <18630816527@163.com>
2025-12-08 19:15:08 +0800

    chore: run the llama demo.

--- python/sglang/srt/layers/attention/flashattention_backend.py
+++ python/sglang/srt/layers/attention/flashattention_backend.py
@@ -693,6 +693,9 @@ class FlashAttentionBackend(AttentionBackend):
     ):
 
         layer_idx = layer.layer_id
+        recent_size = self.duo_attn_config["recent_size"]
+        sink_size = self.duo_attn_config["sink_size"]
+
         print(f"DEBUG: layer type = {type(layer)}")
         print(f"DEBUG: layer is RadixAttention = {type(layer).__name__ == 'RadixAttention'}")
         print(f"DEBUG: layer_idx = {layer_idx}")
@@ -811,84 +814,118 @@ class FlashAttentionBackend(AttentionBackend):
                 num_retrieval_heads = retrieval_idx.stop - retrieval_idx.start
                 num_streaming_heads = streaming_idx.stop - streaming_idx.start
 
-
-                q_view = q.contiguous().view(-1, layer.tp_q_head_num, layer.head_dim)
-
-                # q_retrieval = q_view[:, retrieval_idx, :]
-                # q_streaming = q_view[:, streaming_idx, :]
-
-                q_retrieval = q_view[:, retrieval_idx.start:retrieval_idx.stop, :]
-                q_streaming = q_view[:, streaming_idx.start:streaming_idx.stop, :]
-
                 gqa_group_size = layer.tp_q_head_num // layer.tp_k_head_num
 
-                if gqa_group_size == 1:
-                    kv_retrieval_start = retrieval_idx.start
-                    kv_retrieval_stop = retrieval_idx.stop
-                    kv_streaming_start = streaming_idx.start
-                    kv_streaming_stop = streaming_idx.stop
+
+                if num_retrieval_heads == 0:
+                    result, _ = cute_streaming_sparse_attn_with_kv_cache(
+                        q=q.contiguous().view(-1, layer.tp_q_head_num, layer.head_dim),
+                        k=key_cache,
+                        v=value_cache,
+                        cu_seqlens_q=cu_seqlens_q,
+                        cu_seqlens_k=None,
+                        seqused_k=cache_seqlens,
+                        page_table=page_table,
+                        softmax_scale=layer.scaling,
+                        causal=causal,
+                        window_size=(recent_size, 0),
+                        learnable_sink=None,
+                        sink_size=sink_size,
+                        enable_streaming=True,
+                        softcap=layer.logit_cap,
+                        pack_gqa=True if gqa_group_size > 1 else False,
+                        groupwise=False,
+                        position_ids=None,
+                        m_block_size=128,
+                        n_block_size=128,
+                    )
+                elif num_streaming_heads == 0:
+                    result = flash_attn_with_kvcache(
+                        q=q.contiguous().view(-1, layer.tp_q_head_num, layer.head_dim),
+                        k_cache=key_cache,
+                        v_cache=value_cache,
+                        page_table=page_table,
+                        cache_seqlens=cache_seqlens,
+                        cu_seqlens_q=cu_seqlens_q,
+                        cu_seqlens_k_new=cu_seqlens_k if not use_local_attn else None,
+                        max_seqlen_q=max_seqlen_q,
+                        softmax_scale=layer.scaling,
+                        causal=False if use_cascade_attn else causal,
+                        window_size=window_size,
+                        softcap=layer.logit_cap,
+                        k_descale=k_descale,
+                        v_descale=v_descale,
+                        return_softmax_lse=use_cascade_attn,
+                        **kwargs,
+                    )
                 else:
-                    kv_retrieval_start = retrieval_idx.start // gqa_group_size
-                    kv_retrieval_stop = retrieval_idx.stop // gqa_group_size
-                    kv_streaming_start = streaming_idx.start // gqa_group_size
-                    kv_streaming_stop = streaming_idx.stop // gqa_group_size
+                    q_view = q.contiguous().view(-1, layer.tp_q_head_num, layer.head_dim)
+                    q_retrieval = q_view[:, retrieval_idx.start:retrieval_idx.stop, :]
+                    q_streaming = q_view[:, streaming_idx.start:streaming_idx.stop, :]
 
-                k_cache_ret = key_cache[:, :, kv_retrieval_start:kv_retrieval_stop, :]
-                v_cache_ret = value_cache[:, :, kv_retrieval_start:kv_retrieval_stop, :]
+                    if gqa_group_size == 1:
+                        kv_retrieval_start = retrieval_idx.start
+                        kv_retrieval_stop = retrieval_idx.stop
+                        kv_streaming_start = streaming_idx.start
+                        kv_streaming_stop = streaming_idx.stop
+                    else:
+                        kv_retrieval_start = retrieval_idx.start // gqa_group_size
+                        kv_retrieval_stop = retrieval_idx.stop // gqa_group_size
+                        kv_streaming_start = streaming_idx.start // gqa_group_size
+                        kv_streaming_stop = streaming_idx.stop // gqa_group_size
 
-                k_cache_streaming = key_cache[:, :, kv_streaming_start:kv_streaming_stop, :]
-                v_cache_streaming = value_cache[:, :, kv_streaming_start:kv_streaming_stop, :]
+                    k_cache_ret = key_cache[:, :, kv_retrieval_start:kv_retrieval_stop, :]
+                    v_cache_ret = value_cache[:, :, kv_retrieval_start:kv_retrieval_stop, :]
 
-                o_retrieval, lse_retrieval, *rest_retrieval = flash_attn_with_kvcache(
-                    q=q_retrieval,
-                    k_cache=k_cache_ret,
-                    v_cache=v_cache_ret,
-                    page_table=page_table,
-                    cache_seqlens=cache_seqlens,
-                    cu_seqlens_q=cu_seqlens_q,
-                    cu_seqlens_k_new=cu_seqlens_k if not use_local_attn else None,
-                    max_seqlen_q=max_seqlen_q,
-                    softmax_scale=layer.scaling,
-                    causal=False if use_cascade_attn else causal,
-                    window_size=(1, -1),
-                    softcap=layer.logit_cap,
-                    k_descale=None,
-                    v_descale=None,
-                    return_softmax_lse=True,
-                    **kwargs,
-                )
+                    k_cache_streaming = key_cache[:, :, kv_streaming_start:kv_streaming_stop, :]
+                    v_cache_streaming = value_cache[:, :, kv_streaming_start:kv_streaming_stop, :]
 
-                sink_size = self.duo_attn_sink_size
-                streaming_window = self.duo_attn_streaming_window
+                    o_retrieval, lse_retrieval, *rest_retrieval = flash_attn_with_kvcache(
+                        q=q_retrieval,
+                        k_cache=k_cache_ret,
+                        v_cache=v_cache_ret,
+                        page_table=page_table,
+                        cache_seqlens=cache_seqlens,
+                        cu_seqlens_q=cu_seqlens_q,
+                        cu_seqlens_k_new=cu_seqlens_k if not use_local_attn else None,
+                        max_seqlen_q=max_seqlen_q,
+                        softmax_scale=layer.scaling,
+                        causal=False if use_cascade_attn else causal,
+                        window_size=(1, -1),
+                        softcap=layer.logit_cap,
+                        k_descale=None,
+                        v_descale=None,
+                        return_softmax_lse=True,
+                        **kwargs,
+                    )
+                    o_streaming, lse_streaming = cute_streaming_sparse_attn_with_kv_cache(
+                        q=q_streaming,
+                        k=k_cache_streaming,
+                        v=v_cache_streaming,
+                        cu_seqlens_q=cu_seqlens_q,
+                        # cu_seqlens_k=cu_seqlens_k,
+                        cu_seqlens_k=None,
+                        seqused_k=cache_seqlens,
+                        page_table=page_table,
+                        softmax_scale=layer.scaling,
+                        causal=causal,
+                        window_size=(recent_size, 0),
+                        learnable_sink=None,
+                        sink_size=sink_size,
+                        enable_streaming=True,
+                        softcap=layer.logit_cap,
+                        pack_gqa=True if gqa_group_size > 1 else False,
+                        groupwise=False,
+                        position_ids=None,
+                        m_block_size=128,
+                        n_block_size=128,
+                    )
 
-                o_streaming, lse_streaming = cute_streaming_sparse_attn_with_kv_cache(
-                    q=q_streaming,
-                    k=k_cache_streaming,
-                    v=v_cache_streaming,
-                    cu_seqlens_q=cu_seqlens_q,
-                    # cu_seqlens_k=cu_seqlens_k,
-                    cu_seqlens_k=None,
-                    seqused_k=cache_seqlens,
-                    page_table=page_table,
-                    softmax_scale=layer.scaling,
-                    causal=causal,
-                    window_size=(streaming_window, 0),
-                    learnable_sink=None,
-                    sink_size=sink_size,
-                    enable_streaming=True,
-                    softcap=layer.logit_cap,
-                    pack_gqa=True if gqa_group_size > 1 else False,
-                    groupwise=False,
-                    position_ids=None,
-                    m_block_size=128,
-                    n_block_size=128,
-                )
+                    o = torch.empty_like(q_view)
+                    o[:, retrieval_idx.start:retrieval_idx.stop, :] = o_retrieval
+                    o[:, streaming_idx.start:streaming_idx.stop, :] = o_streaming
 
-                o = torch.empty_like(q_view)
-                o[:, retrieval_idx.start:retrieval_idx.stop, :] = o_retrieval
-                o[:, streaming_idx.start:streaming_idx.stop, :] = o_streaming
-
-                result = o
+                    result = o
             
             else:
                 result = flash_attn_with_kvcache(
